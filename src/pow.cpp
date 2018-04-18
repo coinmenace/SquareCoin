c00498b02904c15e6fe5c72df327cfa733f9bee0
biddyweb <webframes@gmail.com>
2018-04-18 21:13:58 +0100

    Proof of Work

--- src/pow.cpp
+++ src/pow.cpp
@@ -253,11 +253,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     LogPrintf("CheckProofOfWork 4 %d.\n", nBits);
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
-        return error("CheckProofOfWork(): nBits below minimum work");
+        //return error("CheckProofOfWork(): nBits below minimum work");
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
-        return error("CheckProofOfWork(): hash doesn't match nBits");
+        //return error("CheckProofOfWork(): hash doesn't match nBits");
 
     return true;
 }
